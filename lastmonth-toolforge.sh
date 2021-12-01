cf01c30673f0d0e5b1cbf13bc6fb2726b7e135d9
jpxg <x@localhost.localdomain>
2021-12-01 04:17:41 -0800

    fix render output

--- lastmonth-toolforge.sh
+++ lastmonth-toolforge.sh
@@ -87,11 +87,11 @@ goback=$(($DAYS+$daysback))
 
 cd /data/project/jpxg-test/oraclefordeletion
 
-python3 render.py               -a -b $DAYS   -l $YYYY-$MM-$DAYS -o render.txt
-python3 upload.py     			   -o User:JPxG/Oracle/$YYYY-$MM -n "Updating from Toolforge."
+python3 render.py -a -b $DAYS -l $YYYY-$MM-$DAYS -o render3.txt
+python3 upload.py -i render3.txt -o User:JPxG/Oracle/$YYYY-$MM -n "Updating from Toolforge."
 # Render and upload the monthly page.
 
-rm render.txt
+rm render3.txt
 
 #python3 detail.py                  -v -b 1 -l 2014-10-05 -s 0.01
 #python3 detailpages.py             -v -b 1 -l 2014-10-05 -s 0.01
